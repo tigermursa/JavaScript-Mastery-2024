22397ac1c988dd91aaaa71f2abb78a45a6b54358
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-24 13:08:18 +0600

    "done Datatype chapter"

---
+++ Basic/2.Data-type.js
@@ -0,0 +1,29 @@
+/*
+
+There are mainly two type of data in JavaScript
+1)Primitive Data 
+2)Reference Data
+
+Primitive Type data:
+1)Number = 1 2 3 4 5 6 ...
+2)String = "1","Hello","String-words"
+3)Boolean = true false
+4)Undefine 
+5)Null
+6)symbol
+7)BigInt = 90071992547409913
+
+Reference Type data:
+1)Array
+2)Object
+3)Date()
+4)Function
+5)map,set
+
+
+
+BigInt is particularly useful in scenarios where precision matters, and you need to work with integers
+that exceed the limitations of regular JavaScript numbers. It's important to note that not all operations 
+available for regular numbers are available for BigInt. For example, bitwise operations are not supported on BigInt
+
+*/
