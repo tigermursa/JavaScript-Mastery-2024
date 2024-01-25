d16d620dc3093b56d7d61c5b15bafb4032ad6aa3
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-25 21:23:52 +0600

    "Conditional operator done"

---
+++ Basic/4.Conditional-Oprtator.js
@@ -0,0 +1,11 @@
+/*
+?Ternary operator
+The conditional operator, also known as the ternary operator, is a concise way to write conditional statements in JavaScript. 
+It provides a shorthand syntax for expressing simple if-else statements.
+
+condition ? expression_if_true : expression_if_false;
+*/
+
+const age = 10;
+const message = age >= 18 ? "You are an adult!" : "You are a minor!";
+console.log(message); // Output: 'You are an minor!'
