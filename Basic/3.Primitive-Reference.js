d550795e97a8e98a0153823a2f41aa620513c8ec
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-25 21:12:19 +0600

    "updated primitive and red type"

--- Basic/3.Primitive-Reference.js
+++ Basic/3.Primitive-Reference.js
@@ -1,11 +1,13 @@
 /*
 
 Primitive :
-1)Immutable
-2)Stored by value
+1)Immutable: Data type whose state cannot be changed after creation.
+2)Stored by value :Actual value is stored in memory.
 
 Reference :
-1)Mutable
-2)Stored by reference
+1)Mutable :  Data type that can be modified after creation.
+2)Stored by reference : Objects, arrays.
+
+Primitive :
 
 */
