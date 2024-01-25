035d1f00f08dd427ccd2f7304ec2e7cfac8cbd6b
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-25 22:39:21 +0600

    "for loop done"

---
+++ Basic/5.For-while-loop.js
@@ -0,0 +1,13 @@
+/*
+for (initialization; condition; iteration) {
+  // code to be executed in each iteration
+}
+*/
+
+//for loop:
+
+const arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
