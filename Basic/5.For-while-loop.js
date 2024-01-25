96c6af3fea7a41217755b013e14ab569b09b64cc
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-25 22:43:04 +0600

    "while loop done"

--- Basic/5.For-while-loop.js
+++ Basic/5.For-while-loop.js
@@ -11,3 +11,23 @@ const arr = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
+
+//while  loop:
+
+/*
+// Initialization
+let initialization = initial_value;
+
+// While loop
+while (condition) {
+  // Code to be executed in each iteration
+
+  // Iteration expression (update the loop control variable)
+  iteration_expression;
+}
+*/
+let i = 0;
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
