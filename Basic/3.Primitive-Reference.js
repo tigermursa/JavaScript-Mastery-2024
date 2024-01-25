e5752680309bd93a5a3e39b29b97fa98597cbd60
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-25 21:15:12 +0600

    "updated again"

--- Basic/3.Primitive-Reference.js
+++ Basic/3.Primitive-Reference.js
@@ -1,13 +1,17 @@
 /*
 
 Primitive :
+Definition: Basic building blocks of data, like simple values. Examples include numbers, strings, and booleans.
+
 1)Immutable: Data type whose state cannot be changed after creation.
 2)Stored by value :Actual value is stored in memory.
 
 Reference :
+Definition: Complex data types that refer to a location in memory rather than holding the actual value directly.
+Examples include objects, arrays, and functions.
 1)Mutable :  Data type that can be modified after creation.
-2)Stored by reference : Objects, arrays.
+2)Stored by reference : Memory reference is stored, not the actual value.
 
-Primitive :
+
 
 */
