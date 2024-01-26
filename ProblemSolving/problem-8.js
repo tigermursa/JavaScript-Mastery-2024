c5c127ab4bd15ab6bffe539205825c5ab55d8a94
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-26 12:44:38 +0600

    "added problem 8"

---
+++ ProblemSolving/problem-8.js
@@ -0,0 +1,9 @@
+/*
+Write a function to flatten a nested array. The input array can contain nested arrays, 
+and the output should be a single-level array with all the elements.
+
+
+
+Recursion: To handle nested arrays, a recursive approach is often used to traverse through all levels of nesting.
+Array Methods: Utilize array methods such as Array.isArray() and Array.prototype.concat().
+*/
---
+++ ProblemSolving/problem-9.js
@@ -0,0 +1,10 @@
+/*
+Implement a debounce function that delays invoking a function until after a certain amount of time has elapsed since 
+the last time the function was invoked. This is often used to optimize performance by reducing the frequency of execution for 
+functions that are called frequently, such as event handlers.
+
+
+Closures: To maintain the state of the last invocation time.
+setTimeout() and clearTimeout(): To manage the delay between function invocations.
+Higher-Order Functions: Returning a function from another function, creating a debounced version.
+*/
