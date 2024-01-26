f8b5715a403a761e8dbf16c07c2afc1e92f3af40
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-26 12:25:01 +0600

    "array 50% done"

---
+++ Basic/7.Arrays.js
@@ -0,0 +1,18 @@
+
+//normal Object :
+
+const thisArray = {
+  0: 4,
+  1: 6,
+  2: 8,
+  3: 10,
+};
+
+//normal Array :
+const nowLookLike = [4, 6, 8, 10];
+
+console.log(thisArray[0]);
+console.log(nowLookLike[0]);
+
+// So in JavaScript, arrays are a special type of object that is used to store an ordered collection of values.
+//An array in JavaScript is an ordered, indexed collection of values that can include various data types.
