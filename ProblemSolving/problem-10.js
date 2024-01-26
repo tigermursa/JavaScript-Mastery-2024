e23679f6ec31fff8c149798f70c8d1887d5a948e
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-26 15:23:44 +0600

    "problem-10 ready"

---
+++ ProblemSolving/problem-10.js
@@ -0,0 +1,7 @@
+/*
+Write a function that calculates the factorial of a given positive integer.
+The factorial of a number is the product of all positive integers up to that number.
+console.log(factorial(5)); // Should return 120 (5 * 4 * 3 * 2 * 1)
+console.log(factorial(0)); // Should return 1 (by convention, the factorial of 0 is 1)
+Example:
+*/
