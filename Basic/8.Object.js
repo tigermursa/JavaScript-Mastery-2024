304d3dc9f85e2a221554eaebb45d23acf2542cff
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-27 12:00:14 +0600

    "added all kind of Objects later I will update this file"

--- Basic/8.Object.js
+++ Basic/8.Object.js
@@ -1,3 +1,5 @@
+// 1)Object Literal ,where I have stored string number boolean object array and null ..
+
 const person = {
   name: "Mursalin Hossain",
   age: 24,
@@ -24,3 +26,29 @@ const person = {
 
 //accessing the object with dot
 console.log(person.education.SSC.institute); //Frost Academy
+
+//2) Array , Array is actually an Object
+
+//3) Function ,Function  is also an Object
+
+//4) Date() , also an Object
+
+/* 5) Map
+const myMap = new Map();
+myMap.set("key1", "value1");
+*/
+
+/* 6) Set:
+const mySet = new Set([1, 2, 3, 4, 5]);
+*/
+
+/* 7) Set: Promise
+const myPromise = new Promise((resolve, reject) => {
+  // Async operations
+});
+*/
+
+/* 7) Set: Error
+const customError = new Error("This is a custom error.");
+});
+*/
