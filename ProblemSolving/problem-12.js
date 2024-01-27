f71407ebbe17c9dd344843bff8fa73285a112aa3
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-27 22:03:55 +0600

    "Done the problem myself that was great"

--- ProblemSolving/problem-12.js
+++ ProblemSolving/problem-12.js
@@ -9,4 +9,21 @@ console.log(countVowels("Hello"));       // Output should be 2 (e and o are vowe
 console.log(countVowels("Programming"));  // Output should be 3 (o, a, and i are vowels)
 */
 
+const vowelsCounter = (string) => {
+  let vowels = 0;
+  let inputString = string.toLowerCase(); //toUpperCase();
+  for (let index = 0; index < inputString.length; index++) {
+    if (
+      inputString[index] == "a" ||
+      inputString[index] == "e" ||
+      inputString[index] == "i" ||
+      inputString[index] == "o" ||
+      inputString[index] == "u"
+    ) {
+      vowels++;
+    }
+  }
+  return vowels;
+};
 
+console.log(vowelsCounter("English"));
