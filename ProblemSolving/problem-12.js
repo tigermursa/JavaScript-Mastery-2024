bf68d3180f43fe5b136cf7cef58819fc4fa6c511
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-27 21:41:25 +0600

    "added the problem 12 in the file"

---
+++ ProblemSolving/problem-12.js
@@ -0,0 +1,21 @@
+/*
+Problem: Count the Vowels
+
+Write a JavaScript function that takes a string as input and returns the count of vowels (a, e, i, o, u) 
+in the given string. Ignore case while counting
+
+// Example usage:
+console.log(countVowels("Hello"));       // Output should be 2 (e and o are vowels)
+console.log(countVowels("Programming"));  // Output should be 3 (o, a, and i are vowels)
+*/
+
+function vowelsCounter(srt) {
+  let vowels = O;
+  for (let i = 0; i < srt.length; i++) {
+    if (srt == a) {
+      vowels++;
+    }
+  }
+}
+const result = vowelsCounter("a");
+console.log();
