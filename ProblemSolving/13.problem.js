4ff7a8411178b743a3e8b6131a4f8debfef130ef
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-30 21:58:21 +0600

    "added problem 13"

---
+++ ProblemSolving/13.problem.js
@@ -0,0 +1,33 @@
+// Find the close number from array
+
+function findClosestTemperature(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  // Initialize the closest temperature to the first element in the array
+  let closestTemperature = arr[0];
+
+  // Iterate through the array to find the closest temperature to 0
+  for (let i = 1; i < arr.length; i++) {
+    // Compare the absolute values to find the closest temperature
+    if (
+      Math.abs(arr[i]) < Math.abs(closestTemperature) ||
+      (Math.abs(arr[i]) === Math.abs(closestTemperature) && arr[i] > 0)
+    ) {
+      closestTemperature = arr[i];
+    }
+  }
+
+  return closestTemperature;
+}
+
+// Example usage:
+const temperatures = [-8, -1, 3, 4, 5, 6];
+const closestTemp = findClosestTemperature(temperatures);
+console.log("Closest Temperature:", closestTemp);
+
+/*Math.abs() is a JavaScript built-in function that returns the absolute value of a number. 
+The absolute value of a number is its distance from zero on the number line, 
+without regard to its sign (positive or negative). So, whether you have a positive or negative number, 
+applying Math.abs() will give you the positive version of that number. */
