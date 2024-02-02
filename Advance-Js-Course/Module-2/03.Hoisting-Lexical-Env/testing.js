2b5b6424a226dbfb3461ee23bf2964d4efca64ee
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-03 00:48:00 +0600

    "done for today"

--- Advance-Js-Course/Module-2/03.Hoisting-Lexical-Env/testing.js
+++ Advance-Js-Course/Module-2/03.Hoisting-Lexical-Env/testing.js
@@ -1,12 +1,7 @@
-function greet(name) {
-    let message = "Hello, ";
-    console.log(message + name);
-  }
-  
-  function sayHello() {
-    let name = "Alice";
-    greet(name);
-  }
-  
-  sayHello();
-  
+{
+  console.log(hey);
+}
+
+{
+  var hey = "hello";
+}
