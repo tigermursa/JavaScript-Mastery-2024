b0910eaedb2057f0cd182199563209a90d77f969
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-03 20:51:10 +0600

    "updated problem"

--- ProblemSolving/13.problem.js
+++ ProblemSolving/13.problem.js
@@ -23,7 +23,7 @@ function findClosestTemperature(arr) {
 }
 
 // Example usage:
-const temperatures = [-8, -1, 3, 4, 5, 7,9,1,2,3,4,5,6];
+const temperatures = [-8, -1, 3, 4, 5, 7,9,1,2,3,4,5,];
 const closestTemp = findClosestTemperature(temperatures);
 console.log("Closest Temperature:", closestTemp);
 
