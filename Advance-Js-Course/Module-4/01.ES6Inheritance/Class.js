badf09f15e6d455977e4e744eff573f090ad9b8a
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-14 11:38:04 +0600

    "CLASS done"

--- Advance-Js-Course/Module-4/01.ES6Inheritance/Class.js
+++ Advance-Js-Course/Module-4/01.ES6Inheritance/Class.js
@@ -1,2 +1,13 @@
+// class expression
 
+const CarClass = class {
+  constructor(brand, price, stock) {
+    this.company = brand;
+    this.dam = price;
+    this.totalStock = stock;
+  }
+};
 
+const Primo = new CarClass("Toyota", 30, 7);
+
+console.log(Primo);
