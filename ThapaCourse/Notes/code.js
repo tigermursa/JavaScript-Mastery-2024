d01ede9b8957fb5b3ee9470eec34745e71117511
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-15 12:26:47 +0600

    "doing new course class on youtube"

---
+++ ThapaCourse/Notes/code.js
@@ -0,0 +1,12 @@
+// what is the type of the null
+
+const person = null;
+console.log(typeof person); // object
+
+// convert that string to a number
+
+let myNumber = "2024";
+
+console.log(typeof myNumber); // string
+console.log(typeof +myNumber); // number
+console.log(typeof Number(myNumber)); // number
