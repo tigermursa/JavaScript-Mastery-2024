6db3614526fe74ee593036c15ff07165d9427a90
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-15 13:53:32 +0600

    "added typeScript file"

---
+++ TypeScript/hello.ts
@@ -0,0 +1,28 @@
+
+//a normal variable
+let hello: string = "kire";
+console.log(hello)
+
+//tye for the object
+type User = {
+    name: string;
+    age: number;
+    isMarried: boolean;
+    bankBalance: null;
+}
+
+//the object
+const Mursalin: User = {
+    name: "Mursalin Hossain",
+    age: 55,
+    isMarried: false,
+    bankBalance: null
+}
+
+console.log(Mursalin)
+
+// Array
+
+let students: string[] = ['Mursalin', "Meraz", "Dhaly"];
+
+console.log(students)
