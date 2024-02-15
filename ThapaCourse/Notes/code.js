055e90dd17b7e2a052a2a28ffd498abdc6decc68
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-15 12:32:51 +0600

    "updated class note"

--- ThapaCourse/Notes/code.js
+++ ThapaCourse/Notes/code.js
@@ -10,3 +10,13 @@ let myNumber = "2024";
 console.log(typeof myNumber); // string
 console.log(typeof +myNumber); // number
 console.log(typeof Number(myNumber)); // number
+
+// convert that number to a string
+
+let myString = 2024;
+console.log("MyString now:", typeof myString); // number
+console.log("MyString now:", typeof String(myString)); // string
+console.log("MyString now:", typeof myString + ""); // number
+console.log("MyString now:", typeof (myString + "")); // string
+console.log("MyString now:", typeof myString.toString ); // function
+console.log("MyString now:", typeof myString.toString() ); // function
