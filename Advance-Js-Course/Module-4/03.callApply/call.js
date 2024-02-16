c47973a0d111108caa16afd2bbb21598b64b3858
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-16 20:40:05 +0600

    "updates the index file"

---
+++ Advance-Js-Course/Module-4/03.callApply/call.js
@@ -0,0 +1,36 @@
+const hanif = {
+  company: "Shohag",
+  serial: "JH",
+  booking: [],
+  book: function (name, id) {
+    console.log(`${name} booked a seat ub ${this.company} ${id}`);
+  },
+};
+
+hanif.book("Mursalin",110)
+
+
+function sum(c, d) {
+    console.log(this);
+    console.log(this.a + this.b + c + d);
+  }
+  
+  //this is call method
+  sum.call({ a: 10, b: 20 }, 30, 40);
+  //this is apply method
+  sum.apply({ a: 10, b: 20 }, [30, 40]);
+  //this is bind method
+  // const theBindFunction = sum.bind({ a: 10, b: 20 }, 30, 40);
+  // theBindFunction()
+  const theBindFunction = sum.bind({ a: 10, b: 20 });
+  theBindFunction(30,40)
+  
+  // function sum2() {
+  //   let theObj = {
+  //     a: 10,
+  //     b: 20,
+  //   };
+  //   console.log(theObj);
+  // }
+  
+  // sum2.call()
