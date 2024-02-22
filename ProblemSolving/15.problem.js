a6462f5a174b764aa57a220f1f63da21ce4bb749
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-22 23:56:18 +0600

    "updated the code"

--- ProblemSolving/15.problem.js
+++ ProblemSolving/15.problem.js
@@ -6,5 +6,6 @@
 2) make a simple string reverse without using for-loop;
 3)make a array reverse ;
 4)make a array reverse  withOut using reverse() method;
+4)make a array reverse  withOut using reverse() method;
 
 */
