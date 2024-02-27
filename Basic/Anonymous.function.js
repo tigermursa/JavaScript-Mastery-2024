bad813d78415736876296107ca3aa3ce9adb3b72
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-27 22:15:29 +0600

    "function file updated"

--- Basic/Anonymous.function.js
+++ Basic/Anonymous.function.js
@@ -20,3 +20,12 @@ const Obj = {
 };
 
 console.log(Obj.myFunc(2));
+
+/* 
+An anonymous function in JavaScript is a function without a specified name. It is defined inline and often used for short-lived or 
+one-time purposes, such as callbacks or object properties. Anonymous functions can be created using function expressions or 
+arrow functions.
+
+An anonymous function in JavaScript is like a secret helper that doesn't have a name. It's created on the spot when you need it 
+and is handy for quick tasks, kind of like a tool you use briefly and then put away
+*/
