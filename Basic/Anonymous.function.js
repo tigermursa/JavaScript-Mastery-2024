b79380a5072d8b879bbc51df3b330685aca4a460
Mursalin Hossain <mursalin2311@gmail.com>
2024-02-27 22:12:27 +0600

    "function file updated"

--- Basic/Anonymous.function.js
+++ Basic/Anonymous.function.js
@@ -4,11 +4,19 @@ function square(num) {
   return num * num;
 }
 
-console.log( "normal function:",square(2));
+console.log("normal function:", square(2));
 
 //this is arrow function
 
 const square2 = (num) => {
   return num * num;
 };
-console.log("arrow function:",square2(2));
+console.log("arrow function:", square2(2));
+
+//anonymous function
+
+const Obj = {
+  myFunc: (num) => num * num,
+};
+
+console.log(Obj.myFunc(2));
