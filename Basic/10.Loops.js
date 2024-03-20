81c0e4a42b979175c4d89578d5b2c496031ba13c
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-20 20:27:28 +0600

    "for anf for in and while loop done"

--- Basic/10.Loops.js
+++ Basic/10.Loops.js
@@ -2,3 +2,27 @@
 // JAVA SCRIPT LOOPS
 // ############################
 
+//1) FOR LOOP
+
+for (let i = 0; i <= 5; i++) {
+  console.log("for", i);
+}
+
+// 2) WHILE LOOP
+
+let i = 0;
+while (i <= 5) {
+  console.log("while", i);
+  i++;
+}
+
+// 3) FOR IN LOOP (used for iterating over object properties):
+const person = {
+  name: "Apple",
+  age: 35,
+  city: "Dhaka Bangladesh",
+};
+
+for (const i in person) {
+  console.log(`${i}: ${person[i]}`);
+}
