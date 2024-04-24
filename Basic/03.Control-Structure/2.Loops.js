eeadd230155c86b2e56b83fce8c0a9917b9307c1
Mursalin Hossain <mursalin2311@gmail.com>
2024-04-24 23:06:59 +0600

    "used for loop"

--- Basic/03.Control-Structure/2.Loops.js
+++ Basic/03.Control-Structure/2.Loops.js
@@ -4,15 +4,22 @@
 
 //1) FOR LOOP
 
-for (let i = 0; i <= 5; i++) {
-  console.log("for", i);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
+const newArray = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) {
+    newArray.push(arr[i]);
+  }
 }
+
+console.log("for loop", newArray);
 
 // 2) WHILE LOOP
 
 let i = 0;
 while (i <= 5) {
-  console.log("while", i);
+  //console.log("while", i);
   i++;
 }
 
@@ -24,18 +31,18 @@ const person = {
 };
 
 for (const i in person) {
-  console.log(`${i}: ${person[i]}`);
+  //console.log(`${i}: ${person[i]}`);
 }
 
 // 4) FOR OF LOOP (used for iterating over iterable objects like arrays):
 const colors = ["red", "green", "blue"];
 for (let color of colors) {
-  console.log(color);
+  //console.log(color);
 }
 
 // forEach Loop (method for arrays)
 const numbers = [1, 2, 3, 4, 5];
 
 numbers.forEach(function (number) {
-  console.log(number);
+  //console.log(number);
 });
