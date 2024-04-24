5769acece83e7f12f9a1ae60200099b730a05271
Mursalin Hossain <mursalin2311@gmail.com>
2024-04-24 22:42:44 +0600

    "ADDED if else "

---
+++ Basic/03.Control-Structure/1.if-else.js
@@ -0,0 +1,24 @@
+let person = "Ali";
+if (person) {
+  console.log("The person is True");
+} else {
+  console.log("The person is False");
+}
+
+// the result will be true because person variable have true vale
+
+let person2 = ""; // empty string not even a space init
+if (person2) {
+  console.log("The person is True");
+} else {
+  console.log("The person is False");
+}
+// the result will be False because person variable have true vale
+
+let age = 18;
+
+if (age < 18) {
+  console.log("YOU CAN'T HAVE NID CARD");
+} else {
+  console.log("GO AND GET YOUR NID CARD");
+}
