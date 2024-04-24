3f99436b7ce9cbf74a6b7b731af14732522f195d
Mursalin Hossain <mursalin2311@gmail.com>
2024-04-24 21:56:24 +0600

    "practicing operators"

--- Basic/3.5 Operators.js
+++ Basic/3.5 Operators.js
@@ -14,13 +14,13 @@ let x = y ** 2; // means y = 3, so 3 * 3 = 9
 //++ Pre increment
 
 let a = 6;
-let b = ++a // 1 + a = 7
+let b = ++a; // 1 + a = 7
 console.log(b); // b = a, so b = 7
 console.log(a); // 7
 
 //++ Post increment
 
 let d = 6;
-let e = d++ // d + 1 = 7
+let e = d++; // d + 1 = 7
 console.log(e); // 6
 console.log(d); // 7
