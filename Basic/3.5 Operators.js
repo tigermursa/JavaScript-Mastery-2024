7d5f60cc28ca723655ce25d336e0ac986af27eb2
Mursalin Hossain <mursalin2311@gmail.com>
2024-04-24 22:16:47 +0600

    " so operator done"

--- Basic/3.5 Operators.js
+++ Basic/3.5 Operators.js
@@ -13,14 +13,38 @@ let x = y ** 2; // means y = 3, so 3 * 3 = 9
 
 //++ Pre increment
 
-let a = 6;
-let b = ++a; // 1 + a = 7
-console.log(b); // b = a, so b = 7
-console.log(a); // 7
+ let v = 9;
+ let s = ++v; // 1 + v , v = 9 so v = 10
+ //console.log(v); //10
+ //console.log(s); //10
+
 
 //++ Post increment
 
-let d = 6;
-let e = d++; // d + 1 = 7
-console.log(e); // 6
-console.log(d); // 7
+let m = 3;
+let n = m++; // m = 3 + 1 = 4
+//console.log(n); // = 3
+//console.log(m); // = 4
+
+//-- Pre decrement
+
+let  r = 7 ;
+let t = --r; // 1 - 7
+
+//console.log(r); //6
+//console.log(t); //6
+
+//-- Post decrement
+
+let  p = 4 ;
+let q = p--; //  4 -1 = 3
+
+console.log(p); //3
+console.log(q); //4
+
+// Remainder % 
+
+let  w = 13;
+let remainder = w % 5;
+
+console.log(remainder);
